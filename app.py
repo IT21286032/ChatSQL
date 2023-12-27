bd70e1a059a3d6fb358f1c56435b1b62fa5aa411
Kajeevan J <99233247+IT21286032@users.noreply.github.com>
2023-12-27 09:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,82 +112,82 @@ class StreamlitChatPack(BaseLlamaPack):
 
 
 
-        uploaded_file = st.file_uploader("Upload your SQLite database file", type=["db", "sqlite"])
-        if uploaded_file:
-            file_content = uploaded_file.read()
-        
-            with sqlite3.connect(":memory:") as conn:
-                sql_database, service_context, engine, inspector, _ = load_db_llm(file_content)
-
-
-
+            uploaded_file = st.file_uploader("Upload your SQLite database file", type=["db", "sqlite"])
+            if uploaded_file:
+                file_content = uploaded_file.read()
+            
+                with sqlite3.connect(":memory:") as conn:
+                    sql_database, service_context, engine, inspector, _ = load_db_llm(file_content)
     
-
-        # Sidebar for database schema viewer
-        st.sidebar.markdown("## Database Schema Viewer")
-
-        # Use the 'inspector' variable here instead of creating a new one
-        if inspector:
-            # Get list of tables in the database
-            table_names = inspector.get_table_names()
-
-            # Sidebar selection for tables
-            selected_table = st.sidebar.selectbox("Select a Table", table_names)
-
-           
+    
+    
         
     
-        # Display the selected table
-        if selected_table:
-            df = get_table_data(selected_table, conn)
-            st.sidebar.text(f"Data for table '{selected_table}':")
-            st.sidebar.dataframe(df)
+            # Sidebar for database schema viewer
+            st.sidebar.markdown("## Database Schema Viewer")
     
-        # Close the connection
-        conn.close()
-                
-        # Sidebar Intro
-        st.sidebar.markdown('## App Created By')
-        st.sidebar.markdown("""
-        Kajeevan Jeyachandran: 
-        [Linkedin](https://www.linkedin.com/in/kajeevanjeyachandran/ """)
-        
-        st.sidebar.markdown('## Other Projects')
-        st.sidebar.markdown("""
-        """)
-        
-        st.sidebar.markdown('## Disclaimer')
-        st.sidebar.markdown("""This application is for demonstration purposes only and may not cover all aspects of real-world data complexities. Please use it as a guide and not as a definitive source for decision-making.""")
+            # Use the 'inspector' variable here instead of creating a new one
+            if inspector:
+                # Get list of tables in the database
+                table_names = inspector.get_table_names()
     
-        if "query_engine" not in st.session_state:  # Initialize the query engine
-            st.session_state["query_engine"] = NLSQLTableQueryEngine(
-                sql_database=sql_database,
-                synthesize_response=True,
-                service_context=service_context
-            )
-
-        for message in st.session_state["messages"]:  # Display the prior chat messages
-            with st.chat_message(message["role"]):
-                st.write(message["content"])
-
-
-        if prompt := st.chat_input(
-            "Enter your natural language query about the database"
-        ):  # Prompt for user input and save to chat history
-            with st.chat_message("user"):
-                st.write(prompt)
-            add_to_message_history("user", prompt)
-
-        # If last message is not from assistant, generate a new response
-        if st.session_state["messages"][-1]["role"] != "assistant":
-            with st.spinner():
-                with st.chat_message("assistant"):
-                    response = st.session_state["query_engine"].query("User Question:"+prompt+". ")
-                    sql_query = f"```sql\n{response.metadata['sql_query']}\n```\n**Response:**\n{response.response}\n"
-                    response_container = st.empty()
-                    response_container.write(sql_query)
-                    # st.write(response.response)
-                    add_to_message_history("assistant", sql_query)
-
+                # Sidebar selection for tables
+                selected_table = st.sidebar.selectbox("Select a Table", table_names)
+    
+               
+            
+        
+            # Display the selected table
+            if selected_table:
+                df = get_table_data(selected_table, conn)
+                st.sidebar.text(f"Data for table '{selected_table}':")
+                st.sidebar.dataframe(df)
+        
+            # Close the connection
+            conn.close()
+                    
+            # Sidebar Intro
+            st.sidebar.markdown('## App Created By')
+            st.sidebar.markdown("""
+            Kajeevan Jeyachandran: 
+            [Linkedin](https://www.linkedin.com/in/kajeevanjeyachandran/ """)
+            
+            st.sidebar.markdown('## Other Projects')
+            st.sidebar.markdown("""
+            """)
+            
+            st.sidebar.markdown('## Disclaimer')
+            st.sidebar.markdown("""This application is for demonstration purposes only and may not cover all aspects of real-world data complexities. Please use it as a guide and not as a definitive source for decision-making.""")
+        
+            if "query_engine" not in st.session_state:  # Initialize the query engine
+                st.session_state["query_engine"] = NLSQLTableQueryEngine(
+                    sql_database=sql_database,
+                    synthesize_response=True,
+                    service_context=service_context
+                )
+    
+            for message in st.session_state["messages"]:  # Display the prior chat messages
+                with st.chat_message(message["role"]):
+                    st.write(message["content"])
+    
+    
+            if prompt := st.chat_input(
+                "Enter your natural language query about the database"
+            ):  # Prompt for user input and save to chat history
+                with st.chat_message("user"):
+                    st.write(prompt)
+                add_to_message_history("user", prompt)
+    
+            # If last message is not from assistant, generate a new response
+            if st.session_state["messages"][-1]["role"] != "assistant":
+                with st.spinner():
+                    with st.chat_message("assistant"):
+                        response = st.session_state["query_engine"].query("User Question:"+prompt+". ")
+                        sql_query = f"```sql\n{response.metadata['sql_query']}\n```\n**Response:**\n{response.response}\n"
+                        response_container = st.empty()
+                        response_container.write(sql_query)
+                        # st.write(response.response)
+                        add_to_message_history("assistant", sql_query)
+    
 if __name__ == "__main__":
-    StreamlitChatPack(run_from_main=True).run()
+        StreamlitChatPack(run_from_main=True).run()
